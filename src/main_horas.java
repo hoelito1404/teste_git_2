ed826a9d142d9a7dfcd88bd849260d021b45c359
Hoelito1404 <hoelito.lima@gmail.com>
2023-05-25 08:07:41 -0400

    Teste de github

---
+++ src/main_horas.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class main_horas {
+
+	public static void main(String[] args) {
+	
+		
+		Scanner sc = new Scanner (System.in);
+		int hora;
+		
+		System.out.println("Quantas horas?");
+		hora = sc.nextInt();
+		
+		if (hora < 12) {
+		
+			System.out.println("bom dia");
+		
+		}
+		else if(hora <18) {
+			System.out.println("boa tarde");
+		}
+		else {
+			System.out.println("boa noite");
+		}
+		
+	}
+}
+	
+	
